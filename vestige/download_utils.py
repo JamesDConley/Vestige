21228dbbe405b3144aa12bcff7dfcb8e3a17cebf
James Conley <jconley1@conncoll.edu>
2020-12-02 19:58:52 -0500

    Download mode on first install, allow running as module

--- vestige/download_utils.py
+++ vestige/download_utils.py
@@ -11,6 +11,12 @@ class DownloadProgressBar(tqdm):
 
 
 def download_url(url, output_path):
+    """Download a URL to a file .
+
+    Args:
+        url (str): URL pointing towards file to download
+        output_path (str): Local path to save the URL's contents to
+    """
     with DownloadProgressBar(unit='B', unit_scale=True,
                              miniters=1, desc=url.split('/')[-1]) as t:
         urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
